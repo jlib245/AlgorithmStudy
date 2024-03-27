73d7f0f337f3574eff678c41859dcb95cc207413
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-03-27 23:08:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -7,11 +7,8 @@ else :
     a[5] = 1
     if N > 5:
         for i in range(6, N+1):
-            if a[i-3] != -1 or a[i-5] != -1:
-                if a[i-3] == -1:
-                    a[i] = a[i-5] + 1
-                elif a[i-5] == -1:
-                    a[i] = a[i-3] + 1
-                else :
-                    a[i] = min(a[i-5], a[i-3]) + 1
+            if a[i-5] != -1:
+                a[i] = a[i-5]+1
+            elif a[i-3] != -1:
+                a[i] = a[i-3]+1
 print(a[N])
