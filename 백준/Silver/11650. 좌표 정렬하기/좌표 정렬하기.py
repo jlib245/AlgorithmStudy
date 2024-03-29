ac8b77d439557339a0d6ec4c4565ac302bdd402d
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-03-29 21:46:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4312 ms, Memory: 44580 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -3,6 +3,4 @@ for i in range(int(input())):
     lst.append(tuple(map(int, input().split())))
 lst.sort()
 for i in lst:
-    for j in range(2):
-        print(i[j], end=' ')
-    print('')
+    print(i[0], i[1])
