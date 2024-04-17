68e95285449d10186a6de1e532b714f170ba964b
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-04-17 12:29:23 +0900

    [Silver IV] Title: 제로, Time: 4012 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+K = int(input())
+
+lst = []
+for i in range(K):
+    num = int(input())
+    if num == 0:
+        lst.pop()
+    else:
+        lst.append(num)
+
+sum_ = 0
+for i in lst:
+    sum_ += i
+
+print(sum_)
