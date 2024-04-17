5ed0ab06f9af83178add24af78c755c2827d887d
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-04-17 13:00:10 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,18 +1,26 @@
-def f(a= list()):
-    a.pop()
-    return(a)
+import sys
+input = sys.stdin.readline
+
+def f(a):
+    return((-a[1],-a[2],-a[3]))
+
 N, K = map(int, input().split())
+
 G = []
 for i in range(N):
     G.append(list(map(int, input().split())))
+
 G.sort(key=f)
+
 R = []
 for i in range(N):
     R.append(G[i][0])
+
 result = R.index(K)
 while 1:
     if G[result][1:] == G[result-1][1:]:
         result -= 1
     else:
         break
+    
 print(result+1)
