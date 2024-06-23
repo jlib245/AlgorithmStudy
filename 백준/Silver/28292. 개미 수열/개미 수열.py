33763143d5fb73becd0706e73808c007861fa6e9
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-06-23 20:58:24 +0900

    [Silver V] Title: 개미 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28292. 개미 수열/개미 수열.py
+++ 백준/Silver/28292. 개미 수열/개미 수열.py
@@ -1,20 +1,7 @@
-from collections import deque
 N = int(input())
-if N > 5:
+if N < 3:
+    print(1)
+elif N < 6:
+    print(2)
+else:
     print(3)
-    exit()
-q = deque([1])
-for i in range(2, N+1):
-    cnt = 0
-    nq = deque()
-    while q:
-        now = q.popleft()
-        cnt = 1
-        while q and q[0] == now:
-            q.popleft()
-            cnt += 1
-        nq.append(now)
-        nq.append(cnt)
-    q = nq
-
-print(max(q))
