5ca9489f2f80d1fff2b086a2225d3629fd947e23
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-06-29 06:14:31 +0900

    [Silver I] Title: 스타트와 링크, Time: 412 ms, Memory: 123848 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,35 +1,20 @@
 import sys
+from itertools import combinations
 input = lambda : sys.stdin.readline().rstrip()
 
-def bt(n : int):
-    if len(set_) == N // 2:
-        res.append(set(set_))
-        return res
-    for i in range(n+1, N):
-        set_.append(i)
-        bt(i)
-        set_.pop()
-    return res
-
 N = int(input())
-matrx = []
-for i in range(N):
-    matrx.append(list(map(int,input().split())))
+lst = [list(map(int, input().split())) for _ in range(N)]
 
 res = []
-set_ = []
-occ = bt(-1)
+for start in combinations(range(N), N//2):
+    link = [i for i in range(N) if not i in start]
+    startScore = 0
+    linkScore = 0
+    for i in range(N//2):
+        for j in range(i+1, N//2):
+            startScore += lst[start[i]][start[j]] + lst[start[j]][start[i]]
+            linkScore += lst[link[i]][link[j]] + lst[link[j]][link[i]]
+    res.append(abs(startScore-linkScore))
 
-res = []
-while occ:
-    a = occ.pop()
-    total1 = 0
-    total2 = 0
-    for i in range(N):
-        for j in range(N):
-            if i in a and j in a:
-                total1 += matrx[i][j]
-            elif not i in a and not j in a:
-                total2 += matrx[i][j]
-    res.append(abs(total1-total2))
-print(min(res))
+ans = min(res)
+print(ans)
