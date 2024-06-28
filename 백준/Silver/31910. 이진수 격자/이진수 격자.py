0d46fadc868e643172674fe99de63b6c6814b3f1
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-06-29 05:19:38 +0900

    [Silver II] Title: 이진수 격자, Time: 32 ms, Memory: 31120 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Silver/31910. 이진수 격자/이진수 격자.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda : sys.stdin.readline()
+
+N = int(input())
+lst = []
+for i in range(N):
+    lst.append(list(map(int,input().split())))
+dp = [[0]*N for _ in range(N)]
+dp[0][0] = lst[0][0]
+for i in range(1, N):
+    dp[0][i] = dp[0][i-1] * 2 + lst[0][i]
+    dp[i][0] = dp[i-1][0] * 2 + dp[i][0]
+for i in range(1, N):
+    for j in range(1, N):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1])*2 + lst[i][j]
+
+ans = dp[N-1][N-1]
+print(ans)
