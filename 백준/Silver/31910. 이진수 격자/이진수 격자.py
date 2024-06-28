cc340db9811e83e961da898b00d16a03806e1e49
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-06-29 05:25:20 +0900

    [Silver II] Title: 이진수 격자, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31910. 이진수 격자/이진수 격자.py
+++ 백준/Silver/31910. 이진수 격자/이진수 격자.py
@@ -9,7 +9,7 @@ dp = [[0]*N for _ in range(N)]
 dp[0][0] = lst[0][0]
 for i in range(1, N):
     dp[0][i] = dp[0][i-1] * 2 + lst[0][i]
-    dp[i][0] = dp[i-1][0] * 2 + dp[i][0]
+    dp[i][0] = dp[i-1][0] * 2 + lst[i][0]
 for i in range(1, N):
     for j in range(1, N):
         dp[i][j] = max(dp[i-1][j], dp[i][j-1])*2 + lst[i][j]
