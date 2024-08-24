b11d958c6c2913317ecfacc457de8d4e142e3e9f
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-08-25 02:19:35 +0900

    [Silver I] Title: 1로 만들기 2, Time: 116 ms, Memory: 120980 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -3,6 +3,7 @@ import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 def bfs(N):
+    visit = [False]*N
     Q = deque([(N, 0, [N])])
     while Q :
         x, d, route= Q.popleft()
@@ -13,9 +14,11 @@ def bfs(N):
             lst.append(x // 3)
         lst.append(x - 1)
         for nx in lst :
-            if nx == 1 :
-                return d+1, route+[nx]
-            Q.append((nx, d+1, route+[nx]))
+            if not visit[nx] :
+                visit[nx] = True
+                if nx == 1 :
+                    return d+1, route+[nx]
+                Q.append((nx, d+1, route+[nx]))
             
 
 N = int(input())
