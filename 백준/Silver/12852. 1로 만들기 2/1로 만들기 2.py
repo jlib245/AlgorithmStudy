b40ec92e307580c287a79639a2ddf5db368ac052
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-08-25 02:16:55 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1820 ms, Memory: 788908 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+def bfs(N):
+    Q = deque([(N, 0, [N])])
+    while Q :
+        x, d, route= Q.popleft()
+        lst = []
+        if x % 2 == 0 :
+            lst.append(x // 2)
+        if x % 3 == 0 :
+            lst.append(x // 3)
+        lst.append(x - 1)
+        for nx in lst :
+            if nx == 1 :
+                return d+1, route+[nx]
+            Q.append((nx, d+1, route+[nx]))
+            
+
+N = int(input())
+if N == 1 :
+    print(0)
+    print(1)
+    exit()
+cnt, route = bfs(N)
+print(cnt)
+print(*route)
