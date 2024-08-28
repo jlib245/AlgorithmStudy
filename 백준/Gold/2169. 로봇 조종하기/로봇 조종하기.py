ebe68c1d7e879be4131d3ee388b6ed16dbc1240b
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-08-28 17:43:11 +0900

    [Gold II] Title: 로봇 조종하기, Time: 232 ms, Memory: 128752 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -3,33 +3,25 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 val = [list(map(int, input().split())) for _ in range(N)]
-dp = [[[0]*2 for _ in range(M)] for _ in range(N)]
-'''
-dp[i][j][k]
-    i : row
-    j : column
-    k : direction
-        k == 0 : right
-        k == 1 : left
-'''
-dp[0][0] = [val[0][0], val[0][0]]
-impossible = -10**5
-for j in range(1, M):
-    dp[0][j][0] = dp[0][j-1][0] + val[0][j]
-    dp[0][j][1] = impossible
-for i in range(1, N):
-    # direction == right
-    for j in range(M):
-        if j == 0 :
-            dp[i][j][0] = max(dp[i-1][j]) + val[i][j]
-        else :
-            dp[i][j][0] = max(max(dp[i-1][j]), dp[i][j-1][0]) + val[i][j]
-    # direction == left
-    for j in range(M-1, -1, -1) :
-        if j == M-1 :
-            dp[i][j][1] = max(dp[i-1][j]) + val[i][j]
-        else :
-            dp[i][j][1] = max(max(dp[i-1][j]), dp[i][j+1][1]) + val[i][j]
+dp = [[0]*M for _ in range(N)]
 
-ans = max(dp[N-1][M-1])
+dp[0][0] = val[0][0]
+for j in range(1, M):
+    dp[0][j] = dp[0][j-1] + val[0][j]
+
+for i in range(1, N):    
+    right = [0]*M
+    right[0] = dp[i-1][0] + val[i][0]
+    for j in range(1, M) :
+        right[j] = max(dp[i-1][j], right[j-1]) + val[i][j]
+    
+    left = [0]*M
+    left[-1] = dp[i-1][-1] + val[i][-1]
+    for j in range(M-2, -1, -1) :
+        left[j] = max(dp[i-1][j], left[j+1]) + val[i][j]
+    
+    for j in range(M):
+        dp[i][j] = max(left[j], right[j])
+
+ans = dp[-1][-1]
 print(ans)
