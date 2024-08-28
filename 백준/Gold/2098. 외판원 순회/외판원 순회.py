c9ec1bdf794ab86f20a00092501d2b8ee8786c73
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-08-29 03:01:59 +0900

    [Gold I] Title: 외판원 순회, Time: 264 ms, Memory: 127508 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N = int(input())
+
+INF = 10**8
+W = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if not W[i][j] :
+            W[i][j] = INF
+
+# dp[i][j] = 이미 방문한 도시들의 집합 i, 현재 있는 도시 j, 지금까지의 최소비용
+dp = [[INF]*N for _ in range(2**N)]
+dp[1][0] = 0
+
+MAX = 2**N-1
+for i in range(1, MAX) :
+    # j == 다음 목적지
+    for j in range(1, N) :
+        if not (i & (1 << j)) :
+            min_ = INF
+            # k == 원래 있던 곳
+            for k in range(N) :
+                if i & (1 << k) :
+                    a = dp[i][k] + W[k][j]
+                    if min_ > a :
+                        min_ = a
+            dp[i|(1 << j)][j] = min_
+
+ans = INF
+for i in range(1, N) : 
+    if ans > dp[MAX][i] + W[i][0] :
+        ans = dp[MAX][i] + W[i][0]
+
+print(ans)
